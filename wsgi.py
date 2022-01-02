1a60c6f7581ab1ec46fa2d34d496eb8637ae6614
Denis Dubov <denisdbv@gmail.com>
2022-01-02 13:49:51 +0300

    fix

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route('/')
 def main():
-    return 'Hello my first flaskk app at AWS BeanStalk!'
+    return 'Hello my first flaskk app at AWS BeanStalk! Denis!'
