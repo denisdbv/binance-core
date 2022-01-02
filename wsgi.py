cb8df420ff94a281f44c667baa98ca2b9e3342b1
Denis Dubov <denisdbv@gmail.com>
2022-01-02 15:33:58 +0300

    fix

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
+import os
+from os import environ, path
+
 application = Flask(__name__)
 
 @application.route('/')
 def main():
-    return 'Hello my first flaskk app at AWS BeanStalk! Denis!'
+    return 'RDS_HOSTNAME={} RDS_PORT={} RDS_DB_NAME={} RDS_USERNAME={} RDS_PASSWORD={}'.format(os.getenv('RDS_HOSTNAME'), os.getenv('RDS_PORT'), os.getenv('RDS_DB_NAME'), os.getenv('RDS_USERNAME'), os.getenv('RDS_PASSWORD'))
