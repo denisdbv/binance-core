e85980c0ebddeb7d65a2d1edd38179b8470e3f8d
Denis Dubov <denisdbv@gmail.com>
2022-01-02 15:52:00 +0300

    fix

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,5 @@ application = Flask(__name__)
 
 @application.route('/')
 def main():
-    return 'RDS_HOSTNAME={} RDS_PORT={} RDS_DB_NAME={} RDS_USERNAME={} RDS_PASSWORD={}'.format(os.getenv('RDS_HOSTNAME'), os.getenv('RDS_PORT'), os.getenv('RDS_DB_NAME'), os.getenv('RDS_USERNAME'), os.getenv('RDS_PASSWORD'))
+    return 'Hello'
+    # return 'RDS_HOSTNAME={} RDS_PORT={} RDS_DB_NAME={} RDS_USERNAME={} RDS_PASSWORD={}'.format(os.getenv('RDS_HOSTNAME'), os.getenv('RDS_PORT'), os.getenv('RDS_DB_NAME'), os.getenv('RDS_USERNAME'), os.getenv('RDS_PASSWORD'))
